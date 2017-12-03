3fdbc0cb58a9a933ea91d76189e293f8f8c7361c
greate <greatwwgg@gmail.com>
2017-12-03 23:26:53 +0200

    CommitSecondTask

---
+++ Downloads/TestTaskRazzhyvyn/SecondTaskDone/src/Index.java
@@ -0,0 +1,44 @@
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.Calendar;
+import java.util.Properties;
+
+
+public class Index {
+
+    private static final String url = "jdbc:mysql://localhost:3306/test";
+    private static final String user = "admin";
+    private static final String password = "";
+
+    public static void main(String args[]) {
+        String query = "select count(*) from book";
+
+        try
+        {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+            query = " insert into book (Name, Surname, Date, City)"
+                    + " values (?, ?, ?, ?)";
+
+            java.util.Properties prop = new Properties();
+            prop.loadFromXML(new FileInputStream("props.xml"));
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString (1, prop.getProperty("Name"));
+            preparedStmt.setString (2, prop.getProperty("Surname"));
+            preparedStmt.setDate   (3, startDate);
+            preparedStmt.setString (4, prop.getProperty("City"));
+            preparedStmt.execute();
+
+            conn.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+
+
+        }
+    }
